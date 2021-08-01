39ead082a523a86f219077745618a95f9946b964
Salikhova Nargiza <snargiza.sn@gmail.com>
2021-08-01 13:48:44 +0500

    Gipatinuza with double

---
+++ src/floutgipotenuza.java
@@ -0,0 +1,9 @@
+public class floutgipotenuza {
+    public static void main(String[] args) {
+        double a = 3.5;
+        double b = 4.8;
+        double c = Math.sqrt(a*a+b*b);
+        double sqrt = Math.sqrt(c);
+          System.out.println("Корен с равен"+sqrt);
+    }
+}
